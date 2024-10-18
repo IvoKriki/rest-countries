5fd400a99bfe15809fdbfe89f0d5d6e7fe9de8fc
Ivo Kriki <ivo.kriki@outlook.com>
2024-10-18 01:00:34 +0100

    Getting all info from external API

--- src/main/java/com/krikiivo/rest/countries/model/Country.java
+++ src/main/java/com/krikiivo/rest/countries/model/Country.java
@@ -1,20 +1,119 @@
 package com.krikiivo.rest.countries.model;
 
 import java.util.List;
+import java.util.Map;
 
 public class Country {
 
     private Name name;
+    private List<String> tld;
+    private String cca2;
+    private String ccn3;
+    private String cca3;
+    private boolean independent;
+    private String status;
+    private boolean unMember;
+    private Map<String, Currency> currencies;
+    private Idd idd;
     private List<String> capital;
+    private List<String> altSpellings;
     private String region;
+    private String subregion;
+    private Map<String, String> languages;
+    private Map<String, Translation> translations;
+    private List<Double> latlng;
+    private boolean landlocked;
+    private int area;
+    private Demonyms demonyms;
+    private String flag;
+    private Maps maps;
     private int population;
-
+    private Car car;
+    private List<String> timezones;
+    private List<String> continents;
+    private Flags flags;
+    private CoatOfArms coatOfArms;
+    private String startOfWeek;
+    private CapitalInfo capitalInfo;
+    
     public Name getName() {
         return name;
     }
 
     public void setName(Name name) {
         this.name = name;
+    }
+
+    public List<String> getTld() {
+        return tld;
+    }
+
+    public void setTld(List<String> tld) {
+        this.tld = tld;
+    }
+
+    public String getCca2() {
+        return cca2;
+    }
+
+    public void setCca2(String cca2) {
+        this.cca2 = cca2;
+    }
+
+    public String getCcn3() {
+        return ccn3;
+    }
+
+    public void setCcn3(String ccn3) {
+        this.ccn3 = ccn3;
+    }
+
+    public String getCca3() {
+        return cca3;
+    }
+
+    public void setCca3(String cca3) {
+        this.cca3 = cca3;
+    }
+
+    public boolean isIndependent() {
+        return independent;
+    }
+
+    public void setIndependent(boolean independent) {
+        this.independent = independent;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public boolean isUnMember() {
+        return unMember;
+    }
+
+    public void setUnMember(boolean unMember) {
+        this.unMember = unMember;
+    }
+
+    public Map<String, Currency> getCurrencies() {
+        return currencies;
+    }
+
+    public void setCurrencies(Map<String, Currency> currencies) {
+        this.currencies = currencies;
+    }
+
+    public Idd getIdd() {
+        return idd;
+    }
+
+    public void setIdd(Idd idd) {
+        this.idd = idd;
     }
 
     public List<String> getCapital() {
@@ -25,12 +124,92 @@ public class Country {
         this.capital = capital;
     }
 
+    public List<String> getAltSpellings() {
+        return altSpellings;
+    }
+
+    public void setAltSpellings(List<String> altSpellings) {
+        this.altSpellings = altSpellings;
+    }
+
     public String getRegion() {
         return region;
     }
 
     public void setRegion(String region) {
         this.region = region;
+    }
+
+    public String getSubregion() {
+        return subregion;
+    }
+
+    public void setSubregion(String subregion) {
+        this.subregion = subregion;
+    }
+
+    public Map<String, String> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(Map<String, String> languages) {
+        this.languages = languages;
+    }
+
+    public Map<String, Translation> getTranslations() {
+        return translations;
+    }
+
+    public void setTranslations(Map<String, Translation> translations) {
+        this.translations = translations;
+    }
+
+    public List<Double> getLatlng() {
+        return latlng;
+    }
+
+    public void setLatlng(List<Double> latlng) {
+        this.latlng = latlng;
+    }
+
+    public boolean isLandlocked() {
+        return landlocked;
+    }
+
+    public void setLandlocked(boolean landlocked) {
+        this.landlocked = landlocked;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+
+    public Demonyms getDemonyms() {
+        return demonyms;
+    }
+
+    public void setDemonyms(Demonyms demonyms) {
+        this.demonyms = demonyms;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public Maps getMaps() {
+        return maps;
+    }
+
+    public void setMaps(Maps maps) {
+        this.maps = maps;
     }
 
     public int getPopulation() {
@@ -41,7 +220,94 @@ public class Country {
         this.population = population;
     }
 
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public List<String> getTimezones() {
+        return timezones;
+    }
+
+    public void setTimezones(List<String> timezones) {
+        this.timezones = timezones;
+    }
+
+    public List<String> getContinents() {
+        return continents;
+    }
+
+    public void setContinents(List<String> continents) {
+        this.continents = continents;
+    }
+
+    public Flags getFlags() {
+        return flags;
+    }
+
+    public void setFlags(Flags flags) {
+        this.flags = flags;
+    }
+
+    public CoatOfArms getCoatOfArms() {
+        return coatOfArms;
+    }
+
+    public void setCoatOfArms(CoatOfArms coatOfArms) {
+        this.coatOfArms = coatOfArms;
+    }
+
+    public String getStartOfWeek() {
+        return startOfWeek;
+    }
+
+    public void setStartOfWeek(String startOfWeek) {
+        this.startOfWeek = startOfWeek;
+    }
+
+    public CapitalInfo getCapitalInfo() {
+        return capitalInfo;
+    }
+
+    public void setCapitalInfo(CapitalInfo capitalInfo) {
+        this.capitalInfo = capitalInfo;
+    }
+
     public static class Name {
+        private String common;
+        private String official;
+        private Map<String, NativeName> nativeName;
+
+
+        public String getCommon() {
+            return common;
+        }
+
+        public void setCommon(String common) {
+            this.common = common;
+        }
+
+        public String getOfficial() {
+            return official;
+        }
+
+        public void setOfficial(String official) {
+            this.official = official;
+        }
+
+        public Map<String, NativeName> getNativeName() {
+            return nativeName;
+        }
+
+        public void setNativeName(Map<String, NativeName> nativeName) {
+            this.nativeName = nativeName;
+        }
+    }
+
+    public static class NativeName {
         private String common;
         private String official;
 
@@ -59,6 +325,199 @@ public class Country {
 
         public void setOfficial(String official) {
             this.official = official;
+        }
+    }
+
+    public static class Currency {
+        private String name;
+        private String symbol;
+        
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getSymbol() {
+            return symbol;
+        }
+
+        public void setSymbol(String symbol) {
+            this.symbol = symbol;
+        }
+    }
+
+    public static class Idd {
+        private String root;
+        private List<String> suffixes;
+
+        public String getRoot() {
+            return root;
+        }
+
+        public void setRoot(String root) {
+            this.root = root;
+        }
+
+        public List<String> getSuffixes() {
+            return suffixes;
+        }
+
+        public void setSuffixes(List<String> suffixes) {
+            this.suffixes = suffixes;
+        }
+    }
+
+    public static class Translation {
+        private String official;
+        private String common;
+
+        
+        public String getOfficial() {
+            return official;
+        }
+
+        public void setOfficial(String official) {
+            this.official = official;
+        }
+
+        public String getCommon() {
+            return common;
+        }
+
+        public void setCommon(String common) {
+            this.common = common;
+        }
+    }
+
+    public static class Demonyms {
+        private Gender eng;
+
+        public static class Gender {
+            private String f;
+            private String m;
+
+            public String getF() {
+                return f;
+            }
+
+            public void setF(String f) {
+                this.f = f;
+            }
+
+            public String getM() {
+                return m;
+            }
+
+            public void setM(String m) {
+                this.m = m;
+            }
+        }
+
+        public Gender getEng() {
+            return eng;
+        }
+
+        public void setEng(Gender eng) {
+            this.eng = eng;
+        }
+    }
+
+    public static class Maps {
+        private String googleMaps;
+        private String openStreetMaps;
+
+        public String getGoogleMaps() {
+            return googleMaps;
+        }
+
+        public void setGoogleMaps(String googleMaps) {
+            this.googleMaps = googleMaps;
+        }
+
+        public String getOpenStreetMaps() {
+            return openStreetMaps;
+        }
+
+        public void setOpenStreetMaps(String openStreetMaps) {
+            this.openStreetMaps = openStreetMaps;
+        }
+    }
+
+    public static class Car {
+        private List<String> signs;
+        private String side;
+
+        public List<String> getSigns() {
+            return signs;
+        }
+
+        public void setSigns(List<String> signs) {
+            this.signs = signs;
+        }
+
+        public String getSide() {
+            return side;
+        }
+
+        public void setSide(String side) {
+            this.side = side;
+        }
+    }
+
+    public static class Flags {
+        private String png;
+        private String svg;
+
+        public String getPng() {
+            return png;
+        }
+
+        public void setPng(String png) {
+            this.png = png;
+        }
+
+        public String getSvg() {
+            return svg;
+        }
+
+        public void setSvg(String svg) {
+            this.svg = svg;
+        }
+    }
+
+    public static class CoatOfArms {
+        private String png;
+        private String svg;
+
+        public String getPng() {
+            return png;
+        }
+
+        public void setPng(String png) {
+            this.png = png;
+        }
+
+        public String getSvg() {
+            return svg;
+        }
+
+        public void setSvg(String svg) {
+            this.svg = svg;
+        }
+    }
+    
+    public static class CapitalInfo {
+        private List<Double> latlng;
+
+        public List<Double> getLatlng() {
+            return latlng;
+        }
+
+        public void setLatlng(List<Double> latlng) {
+            this.latlng = latlng;
         }
     }
 }
