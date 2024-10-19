71dca5fd260dd9cfc56e502000a5c3d7a2511a96
Ivo Kriki <ivo.kriki@outlook.com>
2024-10-20 00:30:19 +0100

    changed endpoint

--- src/main/java/com/krikiivo/rest/countries/controller/ExternalAPIsController.java
+++ src/main/java/com/krikiivo/rest/countries/controller/ExternalAPIsController.java
@@ -5,16 +5,18 @@ import com.krikiivo.rest.countries.model.Country;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/external")
 public class ExternalAPIsController {
     @Autowired
     private ApiClient apiClient;
 
-    @GetMapping("/consume-api")
+    @GetMapping()
     public ResponseEntity<List<Country>> consumeApi() {
         ResponseEntity<List<Country>> response = apiClient.getData();
         return ResponseEntity.ok(response.getBody());
